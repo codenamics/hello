cf1578d502a3ba892c018052bfbd8720421c9afb
Damian <info@codenamics.pl>
2021-08-20 23:49:52 +0200

    split query

--- API/Data/BoardRepository.cs
+++ API/Data/BoardRepository.cs
@@ -23,10 +23,10 @@ namespace API.Data {
            
         }
         public Task<List<Board>> GetAllBoardsAsync () {
-            return _context.Boards.Include (x => x.Lists).ThenInclude (x => x.Items).ToListAsync ();
+            return _context.Boards.Include (x => x.Lists).ThenInclude (x => x.Items).AsSplitQuery().ToListAsync ();
         }
         public async Task<Board> GetBoardAsync (Guid id) {
-            return await _context.Boards.Include (x => x.Lists.OrderBy(x => x.Order)).ThenInclude (x => x.Items.OrderBy(x => x.Order)).FirstOrDefaultAsync (x => x.Id == id);
+            return await _context.Boards.Include (x => x.Lists.OrderBy(x => x.Order)).ThenInclude (x => x.Items.OrderBy(x => x.Order)).AsSplitQuery().FirstOrDefaultAsync (x => x.Id == id);
         }
         public async Task<bool> SaveChanges () {
             return await _context.SaveChangesAsync () > 0;
