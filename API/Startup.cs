313a3ed8ce88ea73134a6f3f0f1d2c4f4ac74ebc
Damian <info@codenamics.pl>
2021-08-22 23:43:57 +0200

    prod

--- API/Startup.cs
+++ API/Startup.cs
@@ -79,11 +79,7 @@ namespace API {
             app.UseHttpsRedirection ();
 
             app.UseRouting ();
-             app.UseCors(x => x.AllowAnyHeader()
-      
-            .AllowAnyMethod()
-            .AllowCredentials()
-            .WithOrigins("http://localhost:4200"));
+             
 
             app.UseAuthorization ();
 
