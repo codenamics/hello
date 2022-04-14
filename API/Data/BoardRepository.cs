f69750277f2f343959854d9cb548e6d045957df5
Codenamics <26750398+codenamics@users.noreply.github.com>
2022-04-14 02:06:54 +0200

    update dep to .net 6

--- API/Data/BoardRepository.cs
+++ API/Data/BoardRepository.cs
@@ -8,37 +8,46 @@ using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Data {
-    public class BoardRepository : IBoardRepository {
+namespace API.Data
+{
+    public class BoardRepository : IBoardRepository
+    {
         private ApplicationDBContext _context;
-        public BoardRepository (ApplicationDBContext context) {
+        public BoardRepository(ApplicationDBContext context)
+        {
             _context = context;
         }
 
-        public void CreateBoard (Board board) {
-            _context.Boards.Add (board);
+        public void CreateBoard(Board board)
+        {
+            _context.Boards.Add(board);
         }
-        public void DeleteBoard (Board board) {
-            _context.Boards.Remove (board);
+        public void DeleteBoard(Board board)
+        {
+            _context.Boards.Remove(board);
 
         }
-        public Task<List<Board>> GetAllBoardsAsync () {
-            return _context.Boards.Include (x => x.Lists).ThenInclude (x => x.Cards).ToListAsync ();
+        public Task<List<Board>> GetAllBoardsAsync()
+        {
+            return _context.Boards.Include(x => x.Lists).ThenInclude(x => x.Cards).AsSplitQuery().ToListAsync();
         }
-        public async Task<Board> GetBoardAsync (Guid id) {
+        public async Task<Board> GetBoardAsync(Guid id)
+        {
 
-            
-            return await _context.Boards.Include (x => x.Lists.OrderBy (x => x.Order))
-                .ThenInclude (x => x.Cards.OrderBy (x => x.Order))
-                .Where (x => x.Id == id).FirstOrDefaultAsync ();
+
+            return await _context.Boards.Include(x => x.Lists.OrderBy(x => x.Order))
+                .ThenInclude(x => x.Cards.OrderBy(x => x.Order))
+                .Where(x => x.Id == id).AsSplitQuery().FirstOrDefaultAsync();
         }
-        public async Task<bool> SaveChanges () {
-            return await _context.SaveChangesAsync () > 0;
+        public async Task<bool> SaveChanges()
+        {
+            return await _context.SaveChangesAsync() > 0;
 
         }
-        public void UpdateBoard (Board board) {
+        public void UpdateBoard(Board board)
+        {
 
-            _context.Entry (board).State = EntityState.Modified;
+            _context.Entry(board).State = EntityState.Modified;
         }
     }
 }
