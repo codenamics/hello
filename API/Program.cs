848e6a9288b3100685d9ea67a43b141158623d83
Codenamics <26750398+codenamics@users.noreply.github.com>
2022-04-15 02:47:47 +0200

    migrated to .net 6

--- API/Program.cs
+++ API/Program.cs
@@ -1,40 +1,61 @@
 using System;
 using System.Threading.Tasks;
+using API;
+using API.Data;
+using API.Interfaces;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace API
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+builder.Services.AddAutoMapper(typeof(Startup));
+builder.Services.AddScoped<IBoardRepository, BoardRepository>();
+builder.Services.AddScoped<IListRepository, ListRepository>();
+builder.Services.AddScoped<ICardRepository, CardRepository>();
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
+
+builder.Services.AddDbContext<ApplicationDBContext>(
+    options =>
     {
-        public static async Task Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            using var scope = host.Services.CreateScope();
-            var services = scope.ServiceProvider;
-            try
-            {
-                var context = services.GetRequiredService<ApplicationDBContext>();
-                await context.Database.MigrateAsync();
-               
-            }
-            catch (Exception ex)
-            {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "An error occurred during migration");
-            }
+        options.UseNpgsql(builder.Configuration["ConnectionStrings:DefaultConnection"]);
+    });
+var app = builder.Build();
 
-            await host.RunAsync();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseCors(x => x.AllowAnyHeader()
+
+.AllowAnyMethod()
+.AllowCredentials()
+.WithOrigins("http://localhost:4200"));
+
+app.UseAuthorization();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<ApplicationDBContext>();
+    await context.Database.MigrateAsync();
+
 }
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+await app.RunAsync();
+
